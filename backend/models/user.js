b8035c3c0a46aba43b64726ee3d1eb6d293c9493
mostafa <mostafameerzad@gmail.com>
2024-01-26 18:08:00 +0430

    create user model

---
+++ backend/models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 3, maxlength: 55 },
+  password: { type: String, required: true, min: 5, max: 150 },
+  email: { type: String, minlength: 5, maxlength: 255, unique: true },
+});
+
+const User = mongoose.model("User", userSchema);
+
+function validateUser(user) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(55).required(),
+    password: Joi.string().min(5).max(150).required(),
+    email: Joi.string().email().min(5).max(255),
+  });
+
+  return schema.validate(user);
+}
+function validateAuth(user) {
+  const schema = Joi.object({
+    password: Joi.string().min(5).max(150).required(),
+    email: Joi.string().email().min(5).max(255),
+  });
+
+  return schema.validate(user);
+}
+
+module.exports = { validateAuth, validateUser, User };
