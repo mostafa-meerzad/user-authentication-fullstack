382f62e0fb2c5e476061a19faf0ffb80fab20905
mostafa <mostafameerzad@gmail.com>
2024-01-27 21:01:34 +0430

    BugFix fix the issues that was causing the server to crash

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -3,25 +3,31 @@ const { User, validateAuth } = require("../models/user");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-router.post("/login", async (req, res) => {
+module.exports = router.post("/login", async (req, res) => {
   // todo: check user data
   const { error } = validateAuth(req.body);
   if (error) {
     return res.status(400).send(error.details[0].message);
   }
   // todo: lookup for the user with provided details
-  const user = User.findOne({ email: req.body.email });
+  const user = await User.findOne({ email: req.body.email });
 
   if (!user) {
-    return res.status(400).send("invalid username or password");
+    // return res.status(400).send("invalid email or password");
+    return res.status(400).send("no user found");
   }
   // todo: verify the user password by bcrypt
 
+  console.log(user.password);
+  console.log(user.email);
+  console.log(user.name);
   const validPassword = await bcrypt.compare(req.body.password, user.password);
 
   if (!validPassword) {
-    return res.status(400).send("invalid username or password");
+    return res.status(400).send("invalid email or password");
   }
   const token = jwt.sign({ _id: user._id }, "mySecret");
-  res.header("x-auth-token", token);
+  res
+    .header("x-auth-token", token)
+    .send({ email: user.email, name: user.name });
 });
--- backend/routes/signup.js
+++ backend/routes/signup.js
@@ -5,27 +5,31 @@ const _ = require("lodash");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-router.post("/signup", async (req, res) => {
+module.exports = router.post("/signup", async (req, res) => {
   // data sent from client-side for registering the user
   // todo: validate user data sent from client-side
   // todo: look up for the user if not exists create one
 
   const { error } = validateUser(req.body);
+  // console.log(req.body)
   if (error) {
     return res.status(400).send(error.details[0].message);
   }
+
   let user = await User.findOne({ email: req.body.email });
+
   if (user) {
     return res.status(400).send("user already registered!");
   }
 
   user = new User(_.pick(req.body, ["name", "password", "email"]));
-  const salt = bcrypt.genSalt();
-  user.password = bcrypt.hash(user.password, salt);
+  // user = new User({name: req.body.name, password: req.body.password, email:req.body.email});
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
   await user.save();
 
-  const token = jwt.sign({_id:user._id}, "mySecret")
+  const token = jwt.sign({ _id: user._id }, "mySecret");
 
-//   res.send("user successfully registered");
-res.header("x-auth-token", token)
+  // res.send("user successfully registered");
+  res.header("x-auth-token", token).send(_.pick(req.body, ["name", "email"]));
 });
