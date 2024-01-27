c9f1f771b88ba8cd115e8b3bdd896cfa68dc9387
mostafa <mostafameerzad@gmail.com>
2024-01-27 19:43:53 +0430

    complete user signup and login routes

---
+++ backend/routes/login.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { User, validateAuth } = require("../models/user");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+router.post("/auth", async (req, res) => {
+  // todo: check user data
+  const { error } = validateAuth(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  // todo: lookup for the user with provided details
+  const user = User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return res.status(400).send("invalid username or password");
+  }
+  // todo: verify the user password by bcrypt
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+
+  if (!validPassword) {
+    return res.status(400).send("invalid username or password");
+  }
+  const token = jwt.sign({ _id: user._id }, "mySecret");
+  res.header("x-auth-token", token);
+});
---
+++ backend/routes/signup.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const { User, validateUser } = require("../models/user");
+const _ = require("lodash");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+router.post("/", async (req, res) => {
+  // data sent from client-side for registering the user
+  // todo: validate user data sent from client-side
+  // todo: look up for the user if not exists create one
+
+  const { error } = validateUser(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  let user = await User.findOne({ email: req.body.email });
+  if (user) {
+    return res.status(400).send("user already registered!");
+  }
+
+  user = new User(_.pick(req.body, ["name", "password", "email"]));
+  const salt = bcrypt.genSalt();
+  user.password = bcrypt.hash(user.password, salt);
+  await user.save();
+
+  const token = jwt.sign({_id:user._id}, "mySecret")
+
+//   res.send("user successfully registered");
+res.header("x-auth-token", token)
+});
