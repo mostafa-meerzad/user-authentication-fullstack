3ad25dc31f8f69e72533bb619cd9df4e056e1672
mostafa <mostafameerzad@gmail.com>
2024-01-27 21:03:21 +0430

    BugFix fix the maximum number of characters for the password field in database

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true, minlength: 3, maxlength: 55 },
-  password: { type: String, required: true, min: 5, max: 150 },
+  password: { type: String, required: true, min: 5, max: 1042 },
   email: { type: String, minlength: 5, maxlength: 255, unique: true },
 });
 
@@ -13,7 +13,7 @@ function validateUser(user) {
   const schema = Joi.object({
     name: Joi.string().min(3).max(55).required(),
     password: Joi.string().min(5).max(150).required(),
-    email: Joi.string().email().min(5).max(255),
+    email: Joi.string().email().min(5).max(255).required(),
   });
 
   return schema.validate(user);
@@ -21,7 +21,7 @@ function validateUser(user) {
 function validateAuth(user) {
   const schema = Joi.object({
     password: Joi.string().min(5).max(150).required(),
-    email: Joi.string().email().min(5).max(255),
+    email: Joi.string().email().min(5).max(255).required(),
   });
 
   return schema.validate(user);
