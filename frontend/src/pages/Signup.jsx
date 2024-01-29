fd6b8563cc6f1af02319f45634301d7caa1db9b3
mostafa <mostafameerzad@gmail.com>
2024-01-29 21:35:21 +0430

    complete adding sign-up logic to the sign-up form

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,21 +1,89 @@
+import { useState } from "react";
+
 const Signup = () => {
+  const initialFormState = {
+    name: "",
+    email: "",
+    password: "",
+  }
+
+  const [formData, setFormData] = useState(initialFormState);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // FormData is a built-in object that provides a way to easily construct a list of key/value pairs representing the form entries and their values.
+    // e.target refers to the form element itself FormData automatically collects all the form inputs and their values that has a "name" property
+    // Object.fromEntries takes an iterator and returns a JS object
+    // formData.entries() returns an "iterator"
+    // const formData = new FormData(e.target);
+    // const userData = Object.fromEntries(formData.entries());
+    let userData = formData;
+    fetch("http://localhost:3000/api/users/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    })
+      .then((res) => {
+        if (res.ok) {
+          setFormData(initialFormState)
+          return res.headers.get("x-auth-token");
+        } else {
+          throw new Error("Signup failed");
+        }
+      })
+      .then((token) => {
+        // console.log(token);
+        localStorage.setItem("test-auth-token", token);
+      })
+      .catch((err) => {
+        console.log("ERROR: ", err);
+      });
+  };
+
   return (
     <div>
       <h2>Signup</h2>
       <br />
-      <form method="POST" action="http://localhost:3000/api/users/signup">
+      {/* <form method="POST" action="http://localhost:3000/api/users/signup"> */}
+      <form onSubmit={handleSubmit}>
+        {/* <form> */}
         <label htmlFor="name">
           name
-          <input type="name" name="name" id="name" />
+          <input
+            value={formData.name}
+            type="name"
+            name="name"
+            id="name"
+            onChange={(e) => {
+              setFormData({ ...formData, name: e.target.value });
+            }}
+          />
         </label>
         <label htmlFor="email">
           email
-          <input type="email" name="email" id="email" />
+          <input
+            value={formData.email}
+            type="email"
+            name="email"
+            id="email"
+            onChange={(e) => {
+              setFormData({ ...formData, email: e.target.value });
+            }}
+          />
         </label>
         <br />
         <label htmlFor="password">
           password
-          <input type="text" id="password" name="password" />
+          <input
+            value={formData.password}
+            type="text"
+            id="password"
+            name="password"
+            onChange={(e) => {
+              setFormData({ ...formData, password: e.target.value });
+            }}
+          />
         </label>
         <input type="submit" />
       </form>
