fd565d23c98310357302220b96ce8e04a41d3e5a
mostafa <mostafameerzad@gmail.com>
2024-01-29 19:39:24 +0430

    add cors package and corsOptions to expose auth-token

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,13 @@ const app = express();
 require("./startup/db")();
 const users = require("./routes/signup");
 const login = require("./routes/login");
-
+const cors = require("cors");
+const corsOptions = {
+  origin:"http://localhost:5173",
+  exposedHeaders: ["x-auth-token"]
+}
+app.use(express.json())
+app.use(cors(corsOptions))
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/users", users);
 app.use("/api/users", login);
